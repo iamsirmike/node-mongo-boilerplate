887f42ecbe46dce1063d861bc999c48fbb6f213b
iamsirmike <asamoahmichael77@gmail.com>
2022-09-08 20:19:23 +0000

    modify label

--- src/validators/createUserValidator.ts
+++ src/validators/createUserValidator.ts
@@ -1,12 +1,12 @@
 import joi from "joi";
-import { UserInterface } from "../interfaces/userInterface";
 import { joiValidator } from "../helpers/joiValidator";
+import { UserInterface } from "../interfaces/userInterface";
 
 export const createAccountValidator = async(data: UserInterface) => {
 const joiObject = joi.object({
         username: joi.string().min(4).required().label("Username"),
-        fullname: joi.string().email().required().label("Fullname"),
-        age: joi.number().required().label("Role"),
+        fullname: joi.string().required().label("Fullname"),
+        age: joi.number().required().label("Age"),
         password: joi.string().required().label("Password"),
     });
   const validate = joiValidator(joiObject, data);
