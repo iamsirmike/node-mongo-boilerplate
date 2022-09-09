ed7bbd4adca19aa9a11a9291007c14af1ec110ef
iamsirmike <asamoahmichael77@gmail.com>
2022-09-09 14:42:35 +0000

    add error handling for internal server error

--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
 import dotenv from "dotenv";
 import express, { json } from "express";
 import helmet from "helmet";
+import { StatusCodes } from "http-status-codes";
 import morgan from "morgan";
 
 import userRouter from "./src/routes/authRoute";
 
-
 dotenv.config();
 
+const errorHandling = (err: any, req: any, res: any, next: any) => {
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    msg: err.message,
+    success: false,
+  });
+};
 
 const app = express();
 
@@ -17,5 +23,6 @@ app.use(json());
 
 app.use("/auth", userRouter);
 
+app.use(errorHandling);
 
 export default app;
